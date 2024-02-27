269297f514690680cfdd829282dcf36edbde2c52
islamdev2022 <islam.birouk.2004@gmail.com>
2024-02-27 01:21:36 +0100

    adding the registeration forms to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 import Hero from "@/components/landing/Hero"
+import Competition from "@/components/Forms/Competition"
+import Web2 from "@/components/Forms/Web2"
+import Web3 from "@/components/Forms/Web3"
 
 export default function Home() {
     return (
         <main className="bg-background3 min-h-screen text-white">
             <Hero />
+            <div className="">
+                <Competition/>
+            <Web2></Web2>
+            <Web3></Web3>
+            </div>
+            
         </main>
     )
 }
