c7756eec91bcd77e53d76859a393b5f0d3e593f0
Rami Guessab <ramiguessab94@gmail.com>
2024-04-24 17:22:33 +0100

    added messages realtime

--- components/admin/chat/SendMessageButton.tsx
+++ components/admin/chat/SendMessageButton.tsx
@@ -3,32 +3,81 @@ import React from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { supabase } from "@/lib/backend/client_supabase"
+import { toast } from "sonner"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { twMerge } from "tailwind-merge"
+
+const formSchema = z.object({ message: z.string().min(1, "Required") })
 
 export default function SendMessageButton() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            message: "",
+        },
+    })
     return (
-        <div className="flex flex-row gap-4">
-            <Input />
-            <Button
-                onClick={() => {
-                    supabase
-                        .channel("messages", {
-                            config: { broadcast: { self: true } },
-                        })
-                        .send({
-                            event: "message_sent",
-                            type: "broadcast",
-                            payload: "Hello",
-                        })
-                        .then((res) => {
-                            console.log(res)
-                        })
-                        .catch((err) => {
-                            console.log("there is an error", err)
-                        })
-                }}
+        <Form {...form}>
+            <form
+                className="flex flex-row gap-4"
+                onSubmit={form.handleSubmit(({ message }) => {
+                    toast.promise(
+                        supabase
+                            .channel("messages", {
+                                config: { broadcast: { self: true } },
+                            })
+                            .send({
+                                event: "message_sent",
+                                type: "broadcast",
+                                payload: { data: message },
+                            })
+                            .then((res) => {
+                                if (res === "ok") {
+                                    form.reset()
+                                }
+                            }),
+                        {
+                            success: "Message Sent",
+                            error: "Error",
+                            loading: "Sending",
+                        }
+                    )
+                })}
             >
-                Send
-            </Button>
-        </div>
+                <FormField
+                    control={form.control}
+                    name="message"
+                    render={({ field }) => {
+                        return (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <Input
+                                        placeholder="..."
+                                        {...field}
+                                        className={twMerge(
+                                            form.formState.errors.message
+                                                ? "border-red-800 border-2"
+                                                : ""
+                                        )}
+                                    />
+                                </FormControl>
+                            </FormItem>
+                        )
+                    }}
+                />
+                <Button onClick={() => {}}>Send</Button>
+            </form>
+        </Form>
     )
 }
--- components/admin/chat/messages/Messages.tsx
+++ components/admin/chat/messages/Messages.tsx
@@ -2,11 +2,28 @@
 import { useState, useEffect } from "react"
 import Message from "./Message"
 import React from "react"
+import { supabase } from "@/lib/backend/client_supabase"
 
 export default function Messages() {
     const [messages, setMessages] = useState<string[]>([])
 
-    useEffect(() => {}, [])
+    useEffect(() => {
+        const messagesChannel = supabase
+            .channel("messages")
+            .on<{ data: string }>(
+                "broadcast",
+                { event: "message_sent" },
+                ({ payload }) => {
+                    const { data } = payload
+                    setMessages((prev) => [...prev, data])
+                }
+            )
+            .subscribe()
+
+        return () => {
+            messagesChannel.unsubscribe()
+        }
+    }, [])
     return (
         <>
             {messages.map((message, index) => (
